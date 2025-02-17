d8a2fb41fc4a151da9c5d6c0e01b0237753e6752
UsatiyNyan <us4tiyny4n@gmail.com>
2025-02-17 22:49:52 +0100

    time: template configuration of clock_type

--- include/sl/rt/time.hpp
+++ include/sl/rt/time.hpp
@@ -10,30 +10,34 @@
 #include <utility>
 
 namespace sl::rt {
-using clock_type = std::chrono::steady_clock;
 
+template <typename clock_type = std::chrono::steady_clock>
 struct time_point {
-    clock_type::time_point now;
-    clock_type::duration delta_from_init;
-    clock_type::duration delta;
+    typename clock_type::time_point now;
+    typename clock_type::duration delta_from_init;
+    typename clock_type::duration delta;
 
     auto now_sec() const { return std::chrono::duration<float>{ now.time_since_epoch() }; }
     auto delta_from_init_sec() const { return std::chrono::duration<float>{ delta_from_init }; }
     auto delta_sec() const { return std::chrono::duration<float>{ delta }; }
 };
 
-class time : public meta::unique {
-public:
-    time_point calculate() {
+template <typename clock_type = std::chrono::steady_clock>
+struct time : public meta::unique {
+    time_point<clock_type> calculate() {
         const auto now = clock_type::now();
         const auto delta_from_init = now - init_;
         const auto delta = now - std::exchange(now_, now);
-        return time_point{ .now = now, .delta_from_init = delta_from_init, .delta = delta };
+        return time_point<clock_type>{
+            .now = now,
+            .delta_from_init = delta_from_init,
+            .delta = delta,
+        };
     };
 
 private:
-    std::chrono::steady_clock::time_point init_ = std::chrono::steady_clock::now();
-    std::chrono::steady_clock::time_point now_ = init_;
+    typename clock_type::time_point init_ = std::chrono::steady_clock::now();
+    typename clock_type::time_point now_ = init_;
 };
 
 } // namespace sl::rt
